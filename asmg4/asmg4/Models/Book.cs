227175c8804f9fac40bed56d6c4b5b7693e37a20
tung <adjl1234@rocketmail.com>
2015-05-11 09:39:50 +0700

    fregfer

---
+++ asmg4/asmg4/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asmg4.Models
+{
+    public class Book
+    {
+        public string maSach { get; set; }
+        public string maLoai{ get; set;}
+        public string tenSach { get; set; }
+        public string tomTat { get; set; }
+        public string maTG { get; set; }
+        public string maNXB { get; set; }
+
+        public Book(string maSach, string maLoai, string tenSach, string tomTat, string maTG, string maNXB)
+        {
+            this.maSach = maSach;
+            this.maLoai = maLoai;
+            this.tenSach = tenSach;
+            this.tomTat = tomTat;
+            this.maTG = maTG;
+            this.maNXB = maNXB;
+        }
+    }
+}
