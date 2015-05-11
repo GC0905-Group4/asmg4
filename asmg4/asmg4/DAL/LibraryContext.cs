30f783a144abdb96dd5292bc30513c1afe0502ba
tung <adjl1234@rocketmail.com>
2015-05-11 10:23:23 +0700

    fwegwe

---
+++ asmg4/asmg4/DAL/LibraryContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using asmg4.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace asmg4.DAL
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext()
+            : base("LibraryContext")
+        {
+
+        }
+        public DbSet<Sach> sach { get; set; }
+        public DbSet<LoaiSach> loaiSach { get; set; }
+        public DbSet<NhaXB> nhaXB { get; set; }
+        public DbSet<TacGia> tacGia { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
